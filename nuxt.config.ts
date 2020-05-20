3a438e6037cd6454036862e74a06c86a3b7d2565
覃湘 <qinx@smartsalary.cn>
2020-05-21 05:10:51 +0800

    Revert "Content-Security-Policy"
    This reverts commit bfb09006d7f580c8add5b147d54b9ce69b9f67b1.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,8 +37,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     meta: [
-      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
-      { hid: 'http-equiv', 'http-equiv': 'Content-Security-Policy', content: 'upgrade-insecure-requests' }
+      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
     ],
   },
   /*
